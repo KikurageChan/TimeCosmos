526ebbbf51c92e97a9611d56af844228b90e732e
KikurageChan <cihi14057@g.nihon-u.ac.jp>
2017-03-13 14:32:23 +0900

    first commit

---
+++ TimeCosmos/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TimeCosmos
+//
+//  Created by 木耳ちゃん on 2017/03/13.
+//  Copyright © 2017年 KikurageChan. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
